cce068a16785a7506ce44a8d13f5558fb2f8eca6
Afonso F. Garcia <afonsogarcia@icloud.com>
2014-03-26 18:39:52 +0000

    Modified inspector to allow modifications to superclass parameters

--- src/ist/meic/pa/Inspector.java
+++ src/ist/meic/pa/Inspector.java
@@ -24,47 +24,43 @@ public class Inspector {
         while (true) {
             System.err.print("> ");
             String[] command = System.console().readLine().split(" ");
-            if (command[0].equals("q")) {
-                return;
-            } else if (command[0].equals("i")) {
-                if (command.length != 2) {
-                    System.err.println("Parameter was not specified!");
-                    continue;
-                }
-                inspectValue(command[1]);
-            } else if (command[0].equals("m")) {
-                if (command.length != 3) {
-                    System.err.println("Parameter and/or value was not specified!");
-                    continue;
-                }
-                modifyValue(command[1], command[2]);
-            } else if (command[0].equals("c")) {
-                if (command.length != 2) {
-                    System.err.println("Method was not specified!");
-                    continue;
-                }
+
+            if (command[0].equals("i") && command.length == 2) {
+                inspectValue(command[1], inspectTarget.getClass());
+            } else if (command[0].equals("m") && command.length == 3) {
+                modifyValue(command[1], command[2], inspectTarget.getClass());
+            } else if (command[0].equals("c") && command.length >= 2) {
                 callMethod(command[1], command);
-            } else if (command[0].equals("a")) {
-                Boolean useLibrary = new Boolean(command[1]);
-                PrimitiveWrapper.useApacheLibrary(useLibrary);
-                if (useLibrary) {
-                    System.err.println("Using Apache Commons Lang library");
-                } else {
-                    System.err.println("Using Stack Overflow solution");
-                }
+            } else if (command[0].equals("a") && command.length == 2) {
+                useApacheLibrary(command[1]);
             } else if (command[0].equals("h")) {
-                System.err.println("Available commands:");
-                System.err
-                        .println("* a <value>: Changes if the Apache Commons Lang is used or not. Accepted values: true or false.");
-                System.err.println("* i <parameter>: Displays the current value of a parameter of the object being inspected.");
-                System.err.println("* m <parameter> <value>: Modified the value of a parameter of the object being inspected.");
-                System.err
-                        .println("* c <method> <parameters>: Calls the method <method> with parameters <parameters> on the object being inspected.");
-                System.err.println("* h: Displays the inspector help menu.");
-                System.err.println("* q: Quits the inspector.");
+                printHelp();
+            } else if (command[0].equals("q")) {
+                return;
             } else {
                 System.err.println("Invalid command.");
             }
+        }
+    }
+
+    private void printHelp() {
+        System.err.println("Available commands:");
+        System.err.println("* a <value>: Changes if the Apache Commons Lang is used or not. Accepted values: true or false.");
+        System.err.println("* i <parameter>: Displays the current value of a parameter of the object being inspected.");
+        System.err.println("* m <parameter> <value>: Modified the value of a parameter of the object being inspected.");
+        System.err
+                .println("* c <method> <parameters>: Calls the method <method> with parameters <parameters> on the object being inspected.");
+        System.err.println("* h: Displays the inspector help menu.");
+        System.err.println("* q: Quits the inspector.");
+    }
+
+    private void useApacheLibrary(String command) {
+        Boolean useLibrary = new Boolean(command);
+        PrimitiveWrapper.useApacheLibrary(useLibrary);
+        if (useLibrary) {
+            System.err.println("Using Apache Commons Lang library");
+        } else {
+            System.err.println("Using Stack Overflow solution");
         }
     }
 
@@ -100,10 +96,10 @@ public class Inspector {
             System.err.println("An exception was caught while trying to run the method. Printing it's stack trace.");
             e.printStackTrace();
         } catch (InstantiationException e) {
-            // TODO Auto-generated catch block
+            System.err.println("An exception was caught while trying to run the method. Printing it's stack trace.");
             e.printStackTrace();
         } catch (NoSuchMethodException e) {
-            // TODO Auto-generated catch block
+            System.err.println("An exception was caught while trying to run the method. Printing it's stack trace.");
             e.printStackTrace();
         }
     }
@@ -135,41 +131,57 @@ public class Inspector {
         }
     }
 
-    private void modifyValue(String parameter, String value) {
+    private void modifyValue(String parameter, String value, Class<?> inspectClass) {
         try {
-            Field classField = getField(parameter);
+            Field classField = getField(parameter, inspectClass);
             classField.set(inspectTarget, castValue(value, classField.getType()));
-            printObjectProperties();
         } catch (NoSuchFieldException e) {
-            System.err.println("The class does not have the field " + parameter);
+            if (!(inspectClass.getSuperclass().getName().equals("java.lang.Object"))) {
+                modifyValue(parameter, value, inspectClass.getSuperclass());
+            } else {
+                System.err.println("The class does not have the field " + parameter);
+            }
+            return;
         } catch (IllegalArgumentException e) {
             System.err.println("The arguments passed do not match the declared type.");
+            return;
         } catch (IllegalAccessException e) {
-            System.err.println("");
+            System.err.println("An exception was caught while trying to run the method. Printing it's stack trace.");
+            e.printStackTrace();
+            return;
         } catch (InstantiationException e) {
-            // TODO Auto-generated catch block
+            System.err.println("An exception was caught while trying to run the method. Printing it's stack trace.");
             e.printStackTrace();
+            return;
         } catch (InvocationTargetException e) {
-            // TODO Auto-generated catch block
+            System.err.println("An exception was caught while trying to run the method. Printing it's stack trace.");
             e.printStackTrace();
+            return;
         } catch (NoSuchMethodException e) {
-            // TODO Auto-generated catch block
+            System.err.println("An exception was caught while trying to run the method. Printing it's stack trace.");
             e.printStackTrace();
+            return;
         } catch (SecurityException e) {
-            // TODO Auto-generated catch block
+            System.err.println("An exception was caught while trying to run the method. Printing it's stack trace.");
             e.printStackTrace();
+            return;
         }
+        printObjectProperties();
     }
 
-    private void inspectValue(String parameter) {
+    private void inspectValue(String parameter, Class<?> inspectClass) {
         try {
-            Field classField = getField(parameter);
+            Field classField = getField(parameter, inspectClass);
             System.err.print(Modifier.toString(classField.getModifiers()) + " ");
             System.err.print(classField.getType().toString() + " ");
             System.err.print(classField.getName() + " = ");
             System.err.println(getFieldValue(classField));
         } catch (NoSuchFieldException e) {
-            System.err.println("The class does not have the field " + parameter);
+            if (!(inspectClass.getSuperclass().getName().equals("java.lang.Object"))) {
+                inspectValue(parameter, inspectClass.getSuperclass());
+            } else {
+                System.err.println("The class does not have the field " + parameter);
+            }
             return;
         } catch (SecurityException e) {
             System.err.println("An exception was caught while trying to run the method. Printing it's stack trace.");
@@ -183,8 +195,7 @@ public class Inspector {
         }
     }
 
-    private Field getField(String parameter) throws NoSuchFieldException, SecurityException {
-        Class<?> inspectClass = inspectTarget.getClass();
+    private Field getField(String parameter, Class<?> inspectClass) throws NoSuchFieldException, SecurityException {
         Field classField;
         classField = inspectClass.getDeclaredField(parameter);
         classField.setAccessible(true);
@@ -192,13 +203,24 @@ public class Inspector {
     }
 
     private void printObjectProperties() {
+        Class<?> inspectClass = inspectTarget.getClass();
         try {
-            Class<?> inspectClass = inspectTarget.getClass();
-
             System.err.print(inspectTarget.toString() + " is an instance of class ");
             System.err.println(inspectClass.getName());
             System.err.println("----------");
 
+            printFields(inspectClass);
+        } catch (IllegalArgumentException e) {
+            System.err.println("An exception was caught while trying to run the method. Printing it's stack trace.");
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            System.err.println("An exception was caught while trying to run the method. Printing it's stack trace.");
+            e.printStackTrace();
+        }
+    }
+
+    private void printFields(Class<?> inspectClass) throws IllegalAccessException {
+        while (!inspectClass.getName().equals("java.lang.Object")) {
             Field[] classFields = inspectClass.getDeclaredFields();
 
             for (Field f : classFields) {
@@ -208,12 +230,8 @@ public class Inspector {
                 System.err.print(f.getName() + " = ");
                 System.err.println(getFieldValue(f));
             }
-        } catch (IllegalArgumentException e) {
-            System.err.println("An exception was caught while trying to run the method. Printing it's stack trace.");
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            System.err.println("An exception was caught while trying to run the method. Printing it's stack trace.");
-            e.printStackTrace();
+
+            inspectClass = inspectClass.getSuperclass();
         }
     }
 
---
+++ src/ist/meic/pa/test/TotalCoise.java
@@ -0,0 +1,10 @@
+package ist.meic.pa.test;
+
+public class TotalCoise extends Coise {
+    @SuppressWarnings("unused")
+    private boolean c;
+
+    public void setC(boolean c) {
+        this.c = c;
+    }
+}
